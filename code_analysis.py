4f687d39d44cf71d6b4d1f35f96494e0a34e501c
ana-pantilie <ana.pantilie95@gmail.com>
2019-02-20 18:03:03 +0200

    WIP parse analysis cell

--- code_analysis.py
+++ code_analysis.py
@@ -3,6 +3,7 @@ import tempfile
 import subprocess
 import os
 import sys
+import re
 
 # test run: python code_analysis.py "VMTESTS" "DEFAULT" "tests/ethereum-tests/VMTests/vmArithmeticTest/fibbonacci_unrolled.json"
 # TODO: error handling :)
@@ -19,9 +20,35 @@ def main():
                         krun_output_file.name])
         k_final_config = krun_output_file.read()
         tree = ET.ElementTree(ET.fromstring(k_final_config))
-        # should switch to regex?
-        pcounts = [x for x in tree.getroot().find('analysis').text.split() if x.isdigit()]
-        print(pcounts)
+        coverage_kmap = tree.getroot().find('analysis').text
+        # veeery slow
+        # parser = re.compile(r'(?:\s|.)*(\"coverage\"\s\|->(?:\s+SetItem\s\(\s[0-9]+\s\))+)(?:\s|.)*') 
+        # match = parser.match('abcdefg\n "coverage" |-> SetItem ( 0 ) \n SetItem ( 2 )   \n lalalalala ')
+        # if match:
+        #     print(match.group(1))
+        #     # pcounts = [x for x in match.group(1).split() if x.isdigit()]
+        #     # print(pcounts)
+        # else:
+        #     print("nope")
+        first = coverage_kmap.index('"coverage"') + len('"coverage" |->')
+        setitem_len = len('SetItem ( ')
+        buf = list(coverage_kmap)
+        # print(buf)
+        c = first
+        pcounters = []
+        while buf[c] == '\n' or buf[c] == ' ' or buf[c] == 'S':
+            print(buf[c])
+            if buf[c] == 'S':
+                c = c + setitem_len
+                num = []
+                while buf[c].isdigit():
+                    num.append(buf[c])
+                    c = c + 1
+                pcounters.append(num)
+                c = c + len(' )')
+            if buf[c] == '\n' or buf[c] == ' ':
+                c = c + 1
+        print(pcounters)
     # except:
     finally:
         krun_output_file.close()
